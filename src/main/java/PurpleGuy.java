f5c107730eb0cc2bf58d598575e010d9909325a5
Mehxa <muhammadennaayattulla@gmail.com>
2026-01-21 18:11:09 +0800

    Rename Main Class + Add Greeting

---
+++ src/main/java/PurpleGuy.java
@@ -0,0 +1,9 @@
+public class PurpleGuy {
+    public static void main(String[] args) {
+        String line = "\u2500".repeat(50) + "\n";
+
+        System.out.println("Hello! I'm PurpleGuy\nWhat can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
+
