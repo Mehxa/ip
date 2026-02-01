460d0510e332359a5d1a812ba419028072b28ea5
Mehxa <muhammadennaayattulla@gmail.com>
2026-02-01 15:46:15 +0800

    Add JavaDoc comment to ToDo constructor

--- src/main/java/purpleguy/ToDo.java
+++ src/main/java/purpleguy/ToDo.java
@@ -3,6 +3,10 @@ package purpleguy;
  * Contains the details of a task with only a completion status.
  */
 public class ToDo extends Task {
+    /**
+     * Creates a new ToDo Task using the given name
+     * @param name Name of the task
+     */
     public ToDo(String name) {
         super(name);
     }
